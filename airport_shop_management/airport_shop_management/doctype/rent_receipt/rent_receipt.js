5932feffbff48f92d626ab4a9365717f0bc1948d
geethafrutter <geethanjali@frutterlabs.com>
2024-06-22 12:23:00 +0530

    shop management doctypes added

---
+++ airport_shop_management/airport_shop_management/doctype/rent_receipt/rent_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, geethanjali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Rent Receipt", {
+// 	refresh(frm) {
+
+// 	},
+// });
